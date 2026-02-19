62cf5ad209bd225fe60ddc419c87f149e0aac5b7
0xCUB3 <skula@mit.edu>
2026-02-19 18:20:28 -0500

    update wblock cask to 2.0.0 with versioned URL, livecheck, and zap

--- Casks/wblock.rb
+++ Casks/wblock.rb
@@ -1,12 +1,25 @@
 cask "wblock" do
-  version "latest"
-  sha256 :no_check
+  version "2.0.0"
+  sha256 "7a67045111ff1ba2daa61e3f3f8bc627e1ede90bce8e28eb0e1d6a82cb46315a"
 
-  url "https://github.com/0xCUB3/wBlock/releases/latest/download/wBlock.dmg",
+  url "https://github.com/0xCUB3/wBlock/releases/download/#{version}/wBlock-#{version}.dmg",
       verified: "github.com/0xCUB3/wBlock/"
+
   name "wBlock"
   desc "Safari content blocker for macOS, iOS, and iPadOS"
   homepage "https://github.com/0xCUB3/wBlock"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "wBlock.app"
+
+  zap trash: [
+    "~/Library/Preferences/skula.wBlock.plist",
+    "~/Library/Caches/skula.wBlock",
+    "~/Library/Application Support/wBlock",
+    "~/Library/Saved Application State/skula.wBlock.savedState",
+  ]
 end
